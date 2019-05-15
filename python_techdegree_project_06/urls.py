584c7cb751581c763d5e8104ef8f0ef279dc8df2
David McGarvey <davidjmcgarvey@gmail.com>
2019-05-15 09:01:43 -0700

    add minerals app

--- python_techdegree_project_06/urls.py
+++ python_techdegree_project_06/urls.py
@@ -1,4 +1,4 @@
-"""python_techdegree_06 URL Configuration
+"""python_techdegree_project_06 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
